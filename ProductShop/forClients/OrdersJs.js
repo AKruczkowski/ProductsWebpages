35869ce62f84f724bddb74240bf090614e254714
Adam Kruczkowski <adam.kruczkowski@onedynamics.pl>
2022-05-04 00:23:49 +0200

    Add files via upload

--- ProductShop/forClients/OrdersJs.js
+++ ProductShop/forClients/OrdersJs.js
@@ -5,7 +5,7 @@
 
 const uri = "http://localhost:50586/api/Order";
 var temp = 0;
-
+var nrofProd = 1;
 //////////////////////////////////////////////
 function GetOrdData() //async     // Get Orders
 {
@@ -34,8 +34,9 @@ function GetOrdData() //async     // Get Orders
                +"<td>"+ "<button id = '" + data[i].Order_ID + "' onclick = 'ShowHideEdit(id)'> Edit order"
               //  +
               //  "<button id = '" + data[i].Order_ID + "' onclick = 'deleteItem(id)'> Delete"+"</td>"
-               +"</td>"
-              // +"<td>"+ "<button id = '" + data[i].Product_ID + "' onclick = 'updateItem(id)'> Edit" +"</td>"
+              // +"</td>"
+               //+"<td>"
+               +   "<button id = '" + data[i].Order_ID + "' onclick = 'ShowAddressEdit(id)'> Edit address" +"</td>"
               + "</tr>";
               
               $("#tbnbody").append(tablerow);         
@@ -55,7 +56,11 @@ function ShowHideN()  //shows new form for the new order
   {
     x.style.display = "none"
   }
+  //var n = document.getElementById("numberOfProd");
+  const n = $("numberOfProd");
+  nrofProd = n.val();
 
+  
 }
 function ShowHideEdit(id)  //shows new form for the new order
 {
@@ -89,12 +94,12 @@ function displayCurrentOrder(data)
   {
      var tablerow = "<tr>"
             +  "<td>" + data[i].Product_ID +"</td>"
-            +"<td>"+  "<input type='text' id='add-quantity' placeholder='"+data[i].Quantity+"'>"+ "</input>" +"</td>"
-            + "<td>"+ data[i].Price+"</td>"
-            //+"<td>"+ "<button id = '" + data[i].Price + "' onclick = 'ShowHideEdit(id)'> Edit order"
+            +"<td contenteditable>" + data[i].Quantity +"</td>"
+            + "<td>"+ data[i].Price +"</td>"
+            //+"<td>"+ "<button id = '" + data[i].Product_ID + "' onclick = ''> Edit" + "</td>"
            //  +
            //  "<button id = '" + data[i].Order_ID + "' onclick = 'deleteItem(id)'> Delete"+"</td>"
-            +"</td>"
+                     +//"<td>"+  "<input type='text' id='" + data[i].Product_ID +"' placeholder='"+data[i].Quantity+"'>"+ "</input>" +"</td>"   
            + "</tr>";
            
            $("#edittbbody").append(tablerow);  
@@ -102,24 +107,54 @@ function displayCurrentOrder(data)
   }
 }
 //////////////////////////////////////////////
-function CollectAllDetails()
+function CollectAllDetails() //collects all details in given order and pushes them into a table
 {
   var table = document.getElementById("currentOrder");
-  var data = [];
-  for(var i =1;i<table.rows.length; i ++)
-  {
-    var tableRow = table.rows[i];
-    var rowData = [];
-    for(var j =0;j<tableRow.cells.length;j++)
-    {rowData.push(tableRow.cells[j].innerHTML);}
-    data.push(rowData);
-    console.log(rowData);
-  }
- // table = $('#Orders').tableToJSON();
+  var ndata = [];
 
+  var headers = [];
+  for(var i =0; i<table.rows[0].cells.length;i++)
+    {
+      headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi,'');
+    }
+  for(var i=1; i < table.rows.length; i ++)
+  {
+      var tableRow = table.rows[i];
+      var rowData = {};
+      //console.log(tableRow.cells.length);
+      for(var j =0;j<tableRow.cells.length;j++)
+      {
+        rowData[headers[j]] = tableRow.cells[j].innerHTML;
+        console.log(rowData);
+      }
+    ndata.push(rowData);
+   // console.log("Done");
+  }
+fetchEditedOrder(ndata);
+console.log("point");
 }
+
+function fetchEditedOrder(ndata) //fetch data from CollectAllDetails table
+{
+  const newItem = {
+    OrdernDetails:ndata
+  }
+
+  fetch("http://localhost:50586/api/Order/EditOrder/"+temp,{
+    method:"POST",
+    mode: 'cors',
+    dataType: 'json',
+    headers :
+    {'Content-type' : 'application/json'},
+    body: JSON.stringify(newItem)
+  })
+  .then(response => response.json()) 
+  .then(window.location.reload()) // window.location.reload()
+  .catch(error => console.error('Unable to create order', error));
+}
+
 //////////////////////////////////////////////
- function GetProdData() //async   // get products and put them in the list
+ function GetProdData() //async   // get products and put them into the list
  {
    //let response =
      fetch("http://localhost:50586/api/Products", { //await
@@ -131,11 +166,11 @@ function CollectAllDetails()
     })
     .then(response => {return response.json()})
     //.then(data => {console.log(data);return data})
-   .then(data => {console.log(data);createProductList(data)})
+   .then(data => {console.log(data);createProductList(data); LoadProdToList(data)}) //createInputList(data)
    .catch((error)=> console.error(error))
  }
 
- function createProductList(data)
+ function createProductList(data) //creates drop down list of product
  {
     var x = document.getElementById("prodList");
     for(var i =0; i < data.length;i++)
@@ -147,9 +182,6 @@ function CollectAllDetails()
     }
  }
 //////////////////////////////////////////////
-
-
-
 
  function deleteItem(id)
 {
@@ -189,26 +221,142 @@ function CreateNewOrder()
   .then(response => response.json())   // window.location.reload()
   .catch(error => console.error('Unable to create order', error));
 }
-//  function BindDataToTable(data)
-// {
-//   console.log(data);
 
-//    for(var i =0; i < data.length;i++)
-//    {
-//       var tablerow = "<tr>"
-//              +  "<td>" + data[i].Name +"</td>"
-//              +"<td>"+data[i].Price+"</td>"
-//              +"<td>"+ "<button id = '" + data[i].Product_ID + "' onclick = 'ShowHideNew(id)'> Add to order"+"</td>"
-//             // +"<td>"+ "<button id = '" + data[i].Product_ID + "' onclick = 'updateItem(id)'> Edit" +"</td>"
-//             + "</tr>";
-            
-//             $("#tblbody").append(tablerow);         
-//    }
+function createInputList(data)
+{
+  var ord = document.getElementById("forManyOrderDet");
+    for(var i =0;j<nrofProd;i++)
+    {
+      var tablerow = "<tr>"
+      + "<td>" + "<select name = 'products' id = 'prodList'>" + "</td>"
+      +  "<td contenteditable>" + 0 +"</td>"
+      +"<td contenteditable>"+ 0 +"</td>"
+      //+"<td>"+ "<button id = '" + data[i].Order_ID + "' onclick = 'ShowHideEdit(id)'> Edit order"
+     //  +
+     //  "<button id = '" + data[i].Order_ID + "' onclick = 'deleteItem(id)'> Delete"+"</td>"
+      +"</td>"
+     // +"<td>"+ "<button id = '" + data[i].Product_ID + "' onclick = 'updateItem(id)'> Edit" +"</td>"
+     + "</tr>";
+
+
+   }
+     $("#newInputTable").append(tablerow);    
+
+     var x = document.getElementById("prodList");
+     for(var j =0; j < data.length;j++)
+     {
+       var option = document.createElement("option");
+       option.id = data[j].Product_ID;
+       option.text = data[j].Name;
+       x.add(option);
+     } 
+}
+
+function ShowAddressEdit(id)
+  {
+    temp = id;
+    var x = document.getElementById("AddressCh");
+    if(x.style.display === "none")
+    {
+      x.style.display = "block";
+  
+    } else
+    {
+      x.style.display = "none"
+    }
+  }
+
+  function fetchEditedAddress()
+  {
+    debugger;
+    // addAddressTextbox = $('#add-address');
+    const addAddressTextbox = document.getElementById("addressChange");
+    const newItem = {
+      Order_ID:temp,
+      Address:addAddressTextbox.value
+    }
+    //console.log(addAddressTextbox.val());
+    console.log(addAddressTextbox.value);
+    fetch("http://localhost:50586/api/Order/EditOrderNew/"+temp,{
+      method:"POST",
+      mode: 'cors',
+      dataType: 'json',
+      headers :
+      {'Content-type' : 'application/json'},
+      body: JSON.stringify(newItem)
+    })
+    .then(response => response.json()) 
+   // .then(window.location.reload()) // window.location.reload()
+    .catch(error => console.error('Unable to create order', error));
+  }
+
+function newProductForm()
+{
+  var x = document.getElementById("RandomName");
+  if(x.style.display === "none")
+  {
+    x.style.display = "block";
+
+  } else
+  {
+    x.style.display = "none"
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function LoadProductsForNewDetail()
+// {
+
+//   fetch("http://localhost:50586/api/Products", { //await
+//   method:'GET',
+// mode: 'cors',
+// dataType: 'json',
+// headers :
+// {'Content-type' : 'application/json'}
+// })
+// .then(response => {return response.json()})
+// //.then(data => {console.log(data);return data})
+// .then(data => LoadProdToList(data) ) //createInputList(data)
+// .catch((error)=> console.error(error)) 
 // }
 
+function LoadProdToList(data)
+{
+  var x = document.getElementById("prodListN");
+  for(var i =0; i < data.length;i++)
+  {
+    var option = document.createElement("option");
+    option.id = data[i].Product_ID;
+    option.text = data[i].Name;
+    x.add(option);
+  }
+}
 
+function fetchDetail()
+{
 
+  var x = document.getElementById("prodListN");
+  var n = parseInt(x.options[x.selectedIndex].id);
+  
+  const addQuantityTextbox = $('#add-quantityN');
 
+      const newItem ={
+     Product_ID:n,
+     Quantity:addQuantityTextbox.val()
+      };
+if(temp != 0 && temp != null)
+  fetch("http://localhost:50586/api/Order/AddOrderDetail/"+temp,{
+    method:"POST",
+    mode: 'cors',
+    dataType: 'json',
+    headers :
+    {'Content-type' : 'application/json'},
+    body: JSON.stringify(newItem)
+  })
+  .then(response => response.json())   // window.location.reload()
+  .catch(error => console.error('Unable to create order', error));
+
+  
+}
 
 
 
@@ -226,93 +374,30 @@ function CreateNewOrder()
 
 
 /////////////////////////////////////////////////////////////////////////////////
-  function ShowHideNew(id)
-{
-      // var x = document.getElementById("formNewOrder");
-      // if(x.style.display === "none")
-      // {
-      //   x.style.display = "block";
+//   function ShowHideNew(id)
+// {
+//       // var x = document.getElementById("formNewOrder");
+//       // if(x.style.display === "none")
+//       // {
+//       //   x.style.display = "block";
 
-      // } else
-      // {
-      //  // x.style.display = "none"
-      // }
-    var data = fetch("http://localhost:50586/api/Products/" + id, { //await
-      method:'GET',
-    mode: 'cors',
-    dataType: 'json',
-    headers :
-    {'Content-type' : 'application/json'}
-    })
-    .then(response => {return response.json()})
-    //.then(data => {console.log(data);return data})
-    .then(data => {console.log(data);ShowProd(data)})
-      .catch((error)=> console.error(error));
-      //console.log(id);
-      temp = id;
-      //console.log(temp);
-}
+//       // } else
+//       // {
+//       //  // x.style.display = "none"
+//       // }
+//     var data = fetch("http://localhost:50586/api/Products/" + id, { //await
+//       method:'GET',
+//     mode: 'cors',
+//     dataType: 'json',
+//     headers :
+//     {'Content-type' : 'application/json'}
+//     })
+//     .then(response => {return response.json()})
+//     //.then(data => {console.log(data);return data})
+//     .then(data => {console.log(data);ShowProd(data)})
+//       .catch((error)=> console.error(error));
+//       //console.log(id);
+//       temp = id;
+//       //console.log(temp);
+// }
 /////////////////////////////////////////////////////////////////////////////////////
-
-
-function ShowProd(data)
-{
-      var tablerow = "<tr>"
-      +  "<td>" + data.Name +"</td>"
-      +  "<td>" +"<button id = '" + data.Product_ID + "' onclick = 'buyItem(id)'> Buy" +"</td>"
-      "</tr>";
-      $("#tablewithp").append(tablerow);  
-}
-
-  function buyItem()
-  {
-    const addQuantityTextbox = $('#add-quantity');
-    const newItem ={
-      Product_ID:temp,
-      Address:"Random",
-      Quantity: addQuantityTextbox.val(),
-    };
-
-    fetch(uri,{
-      method:"POST",
-      mode: 'cors',
-      dataType: 'json',
-      headers :
-      {'Content-type' : 'application/json'},
-      body: JSON.stringify(newItem)
-    })
-    .then(response => response.json())   // window.location.reload()
-    .catch(error => console.error('Unable to create order', error));
-  }
-
-
-
-function showDetais(id)
-{
-  fetch("http://localhost:50586/api/Order/" + id, { //await
-  method:'GET',
-mode: 'cors',
-dataType: 'json',
-headers :
-{'Content-type' : 'application/json'}
-})
-.then(response => {return response.json()})
-//.then(data => {console.log(data);return data})
-.then(data => {console.log(data);DetailsinTable(data)})
-.catch((error)=> console.error(error))
-}
-
-function DetailsinTable(data)
-{
-
-  var tablerow = "<tr>"
-   +  "<td>" + data.Name +"</td>"
-   +  "<td>" + data.Product_ID +"</td>"
-   + "<td>" + data.Quantity +"</td>"
-   +  "<td>" + data.Price +"</td>"
-  "</tr>";
-  $("#tablewithp").append(tablerow);  
-}
-
-
-//            <input type="submit" value="Sumbit">
